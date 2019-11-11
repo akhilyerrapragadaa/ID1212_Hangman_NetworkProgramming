e0771c40d957f0f1f52051b59917a9ca4643c485
akhilyerrapragadaa <akhilyerrapragada905@gmail.com>
2019-11-12 00:28:14 +0100

    With length header

--- HangRunnable.java
+++ HangRunnable.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 
 	public class HangRunnable implements Runnable {
@@ -12,6 +13,7 @@ import java.net.Socket;
 		HangmanService hangmanService =  new HangmanService();   
 		protected Socket clientSocket = null;
 		boolean flag = true;
+		boolean datlost;
 		public HangRunnable(Socket clientSocket) {
 			this.clientSocket = clientSocket;
 		}
@@ -23,28 +25,45 @@ import java.net.Socket;
 			try {
 				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-			    String arg1;
+			    String arg;
 			    
-			    while((arg1 = in.readLine())!= "Exit") {
-			    	
-				    if(arg1.equals("Exit")) {
+			    while((arg = in.readLine())!= "Exit") {
+			    	datlost = false;
+			    	String str = arg;
+			    	String[] part = str.split("(?<=\\D)(?=\\d)");
+				    String arg1= part[0];
+				  //  System.out.println(arg);
+				    int bytes = Integer.parseInt(part[1]);
+				    int byto =arg1.getBytes(StandardCharsets.UTF_8).length;
+				   // System.out.println(bytes);
+				    //System.out.println(byto);
+				    //System.out.println(arg1);
+			    	if(arg1.equals("Exit")) {
 				    	
 				    	in.close();
 				    	out.close();
 				    }
 				    else {
 				    	System.out.println("Client Says:"+ arg1);
-					    //out.println("Thanks for the message");
-				    	if(count == 0) {
+					    if(bytes != byto ){
+					    	String tag = "Some data has been lost in the network please resend the word";
+					        out.println(tag);
+					        datlost = true;
+					    }
+				    	//out.println("Thanks for the message");
+					    else if(count == 0) {
 				    		hangmanService.newGame();
 				    		count = 1;
 				    	}
-				    		
+				    	
+					    if(!datlost) {
 				    	String outP = hangmanService.play(arg1);
 				    	out.println(outP);
 				    	if(outP.contains("score"))
 				    		count =0;
-				    }
+					    }
+					    }
+					    
 				    
 			    
 			    }
--- HangmanClient.java
+++ HangmanClient.java
@@ -2,13 +2,18 @@ package com.kth.id1212.hangman;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+
+import javax.sound.sampled.AudioFormat.Encoding;
 
 
 public class HangmanClient {
 	//PrintWriter out;
+	int i=0;
 	public void startConnection(String hostName, int portNumber) {
 		boolean flag = true;
+		//int i=0;
 		// TODO Auto-generated method stub
        //String hostName ="127.0.0.1";
        //int portNumber = 8080;
@@ -23,6 +28,9 @@ public class HangmanClient {
     	   //Create ourIO streams
     	   out = new PrintWriter(clientSocket.getOutputStream(), true);
     	   in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    	   // netStream = clientSocket.getOutputStream();
+    	  // ByteArrayOutputStream byteOutputStream =  new ByteArrayOutputStream();
+    	   //ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOutputStream);
     	   while(flag) {
     	   if(info) {
     	   System.out.println("Please remember that you can exit the game any time by typing - Exit");	   
@@ -31,7 +39,11 @@ public class HangmanClient {
     	   System.out.println("Enter Word/Words:");
 		   Scanner myObj1 = new Scanner(System.in);
 		   String datain = myObj1.nextLine();
-    	   out.println(datain);
+		 //  System.out.println(datain.getBytes(StandardCharsets.UTF_8));
+		   int a =datain.getBytes(StandardCharsets.UTF_8).length;
+		 //  System.out.println(datain.getBytes().length);
+		   out.println(datain+a);
+    	   
     	   System.out.println(in.readLine());
     	   if(datain.equals("Exit")) {
     		   flag = false;
@@ -48,10 +60,13 @@ public class HangmanClient {
        }
        
 	}
-    
-	//public void sendMessage(String datain) {
-		//out.println(datain);
-	//}
+   // int i=0;
+	/*public int frameMsg(byte[] message) {
+	  	  for(byte b:message) {
+	  		  i++;
+	  	  }
+	  	  return i;
+ }*/
 	
 }
 
